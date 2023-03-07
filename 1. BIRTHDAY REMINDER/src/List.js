d3e3c586a2b0d99470aa80d82fbbdaaa819e5dd9
Emre Özdin <mrzdnboun@gmail.com>
2023-03-07 14:15:45 +0300

    gitIgnore

--- 1. BIRTHDAY REMINDER/src/List.js
+++ 1. BIRTHDAY REMINDER/src/List.js
@@ -3,7 +3,15 @@ import React from 'react';
 const List = () => {
   return (
     <>
-      <h2>list component</h2>
+      <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
     </>
   );
 };
