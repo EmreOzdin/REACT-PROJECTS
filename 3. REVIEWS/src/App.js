e06648dea7e9965a25b7f507737882e3980e8010
Emre Özdin <mrzdnboun@gmail.com>
2023-03-10 15:47:51 +0300

    reviewAppFinished

--- 3. REVIEWS/src/App.js
+++ 3. REVIEWS/src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import Review from './Review';
+import { makeStyles } from "@mui/styles";
+import Container from "@mui/material/Container";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  },
+}));
+
 function App() {
-  return <Review />;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Container maxWidth="sm">
+        <Review />
+      </Container>
+    </div>
+  );
 }
 
 export default App;
