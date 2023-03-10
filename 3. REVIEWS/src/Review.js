442a401d638f37ce22b56a700c8c093dd7d67878
Emre Özdin <mrzdnboun@gmail.com>
2023-03-10 13:48:04 +0300

    reviewsAdded_2

--- 3. REVIEWS/src/Review.js
+++ 3. REVIEWS/src/Review.js
@@ -3,7 +3,21 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [random, setRandom] = useState(0);
+  const { id, name, image, job, text } = people[random];
+
+  return (
+    <div>
+      <div key={id}>
+        <h2>{name}</h2>
+        <h2>{job}</h2>
+        <h2>{text}</h2>
+        <img src={image} style={{ width: "25%" }} />
+      </div>
+      <FaChevronLeft onClick={handleLeftClick} />
+      <FaChevronRight onClick={handleRightClick} />
+    </div>
+  );
 };
 
 export default Review;
