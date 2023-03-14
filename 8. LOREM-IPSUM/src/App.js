828119359d9e4d446c01c31d0961771b6079a472
Emre Özdin <mrzdnboun@gmail.com>
2023-03-14 23:58:45 +0300

    sliceMethodAdded

--- 8. LOREM-IPSUM/src/App.js
+++ 8. LOREM-IPSUM/src/App.js
@@ -2,9 +2,15 @@ import React, { useState } from 'react';
 import data from './data';
 
 function App() {
-  const [num, setNum] = useState(5);
-  const [para, setPara] = useState(data.slice(num-1));
-  console.log(para)
+  const [num, setNum] = useState(1);
+  const [para, setPara] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setNum(e.target.value);
+    setPara(data.slice(0,num));
+    console.log(num)
+  }
 
   return (
     <div className="container">
@@ -13,24 +19,25 @@ function App() {
         <div className="align-items-center">
           <span>Paragraphs</span>
         </div>
-        <div className=" gap-3" style={{width: "100px"}}>
+        <div className=" gap-3" style={{ width: "100px" }}>
           <input
             type="number"
             className="form-control"
             id="inputPassword2"
             min={0}
             max={8}
+            step={1}
+            value={num}
+            onChange={(e) => setNum(e.target.value)}
           />
         </div>
         <div className="pt-3">
-          <button type="submit" className="btn btn-primary mb-3">
+          <button type="submit" className="btn btn-primary mb-3" onClick={handleSubmit}>
             GENERATE
           </button>
         </div>
       </form>
-      <div className="row">
-        <p></p>
-      </div>
+      <div className="row">{para}</div>
     </div>
   );
 }
