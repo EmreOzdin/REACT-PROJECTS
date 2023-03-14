66e74d0e2094cdec2c0dab7a8a83d144ffce9956
Emre Özdin <mrzdnboun@gmail.com>
2023-03-14 17:58:46 +0300

    sliderAppAdded

--- 6. TABS/src/App.js
+++ 6. TABS/src/App.js
@@ -1,29 +1,30 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 import axios from "axios";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = "https://course-api.com/react-tabs-project";
 
 function App() {
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
   const [num, setNum] = useState(0);
 
   const handleClick = (id) => {
     setNum(id);
   };
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setData(newJobs);
+  };
   useEffect(() => {
-    fetchData();
-    async function fetchData() {
-      try {
-        const response = await axios.get(url);
-        setData(response.data);
-        console.log(data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
+    fetchJobs();
   }, []);
+console.log(data);
+
+  const { dates, duties, order, title, id} = data[num];
+
   return (
     <div className="section">
       <h1 className="title">Experience</h1>
@@ -33,36 +34,47 @@ function App() {
           {data?.map((item) => {
             return (
               <>
-                <div style={{ display: "flex" }}>
-                  <h2 className="job-btn" onClick={handleClick(item.id)}>
-                    {item.company}
+                <div key={item.id} style={{ display: "flex" }}>
+                  <h2
+                    className="job-btn"
+                    onClick={() => handleClick(item.order)}
+                  >
+                    deneme
                   </h2>
                 </div>
               </>
             );
           })}
-          <div>
-            <h2 className="job-info">{data[num]?.title}</h2>
-            {console.log(data[num])}
-          </div>
+          {console.log(num)}
+
+                <>
+                  <div style={{ display: "flex" }}>
+                    <div>
+                      <h2 className="job-info">{title}</h2>
+                      <h2>{dates}</h2>
+                      <h2>{duties}</h2>
+                    </div>
+                  </div>
+                </>
+
         </div>
       </div>
     </div>
   );
 }
-export default App
+export default App;
 
 //another version of data fetching
 
-  // const fetchData = async () => {
-  //   try {
-  //     const response = await axios.get(url);
-  //     console.log(response.data);
-  //     const data = response.data;
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
+// const fetchData = async () => {
+//   try {
+//     const response = await axios.get(url);
+//     console.log(response.data);
+//     const data = response.data;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+// useEffect(() => {
+//   fetchData();
+// }, []);
